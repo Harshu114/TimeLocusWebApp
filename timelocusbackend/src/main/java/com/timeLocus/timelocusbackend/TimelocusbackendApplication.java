0dbafc52827ea9aecb0ce5543c5b23c0e2de2dd2
Harshu114 <149943261+Harshu114@users.noreply.github.com>
2026-03-16 19:21:21 +0530

    backend initial commits
    created Springboot project for the TimeLocus backend

---
+++ timelocusbackend/src/main/java/com/timeLocus/timelocusbackend/TimelocusbackendApplication.java
@@ -0,0 +1,13 @@
+package com.timeLocus.timelocusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelocusbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelocusbackendApplication.class, args);
+	}
+
+}
---
+++ timelocusbackend/src/test/java/com/timeLocus/timelocusbackend/TimelocusbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeLocus.timelocusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelocusbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
