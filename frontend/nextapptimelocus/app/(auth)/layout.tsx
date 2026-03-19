13a5581a00946edc33df0ad6c2e3861158a8e137
Harshu114 <149943261+Harshu114@users.noreply.github.com>
2026-03-19 16:41:58 +0530

    next js version upgrade

--- frontend/nextapptimelocus/app/(auth)/layout.tsx
+++ frontend/nextapptimelocus/app/(auth)/layout.tsx
@@ -1,4 +1,11 @@
 // app/(auth)/layout.tsx
+import type { Metadata } from 'next';
+import '../globals.css'; // Changed from './' to '../'
+
+export const metadata: Metadata = {
+  title: 'TimeLocus',
+};
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
