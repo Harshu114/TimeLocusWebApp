8f7b3db786bff042ddfad378b3c0fb0eac0579e0
Harshu114 <149943261+Harshu114@users.noreply.github.com>
2026-03-19 17:04:15 +0530

    v1

--- frontend/nextapptimelocus/next.config.mjs
+++ frontend/nextapptimelocus/next.config.mjs
@@ -1,10 +1,8 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
       {
-        // All /api/* calls are forwarded to your Spring Boot backend
         source: '/api/:path*',
         destination: 'http://localhost:8080/api/:path*',
       },
